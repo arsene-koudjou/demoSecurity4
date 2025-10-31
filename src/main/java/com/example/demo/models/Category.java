88bf8c92e41634eeb91a9ab3c5e811e0712da817
DESKTOP-IAOJVJL\Administrateur <arsenekoudjou@gmail.com>
2025-10-31 21:31:29 +0100

    create security 4 project with entities

--- src/main/java/com/example/demo/models/Category.java
+++ src/main/java/com/example/demo/models/Category.java
@@ -3,10 +3,12 @@ package com.example.demo.models;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 @Entity
 public class Category {
     private String name;
